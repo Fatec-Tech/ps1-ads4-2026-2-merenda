34d60e6839cc4efbdd09fc2702f89a644a5050ac
aluno <aluno@fateclins.local>
2026-09-02 22:20:06 -0300

    Aula dia 0209

--- aula-01-cadastro-pacientes/js/app.js
+++ aula-01-cadastro-pacientes/js/app.js
@@ -1,23 +1,43 @@
 // Array que guarda os pacientes cadastrados (em memória, só nesta sessão)
 const pacientes = [];
 
+//Contador de pacientes
+const cont = document.getElementById('cont');
+let cliques = 0;
+
 // Referências aos elementos do DOM que vamos usar várias vezes
 const formulario = document.getElementById('form-paciente');
 const tabela = document.getElementById('tabela-pacientes');
 
 // Função responsável por adicionar um paciente ao array
-function adicionarPaciente(nome, email, nascimento, telefone) {
-  const novoPaciente = { nome, email, nascimento, telefone };
-  console.log(novoPaciente);
-  pacientes.push(novoPaciente);
+function adicionarPaciente(nome, email, nascimento, telefone, idade) {
+  const novoPaciente = { nome, email, nascimento, telefone, idade };
+
+  const duplicado = pacientes.filter(pacientes => pacientes.email === novoPaciente.email);
+
+  if(duplicado.length > 0){
+    alert('Email já cadastrado');
+  }else{
+    console.log(novoPaciente);
+    pacientes.push(novoPaciente);
+  }
+
 }
+
 function calcaularIdade(nascimento){
   const hoje = new Date();
   const nasceu = new Date(nascimento);
 
   let idade = hoje.getFullYear() - nasceu.getFullYear();
 
-  const mesAtual
+  const mesAtual = hoje.getMonth();
+  const mesNasc = nasceu.getMonth();
+
+  if(mesAtual < mesNasc || (mesAtual === mesNasc && hoje.getDay() < nasceu.getDay())){
+    idade--;
+  }
+
+  return idade;
 }
 
 // Função responsável por desenhar a tabela inteira a partir do array
@@ -32,6 +52,8 @@ function renderizarTabela() {
       <td>${paciente.email}</td>
       <td>${formatarData(paciente.nascimento)}</td>
       <td>${paciente.telefone}</td>
+      <td>${paciente.idade}</td>
+      <button type="Apagar" class="btn btn-primary w-100">-</button>
     `;
 
     tabela.appendChild(linha);
@@ -48,12 +70,16 @@ function formatarData(dataISO) {
 formulario.addEventListener('submit', (event) => {
   event.preventDefault(); // evita o recarregamento da página
 
+  cliques++;
+  cont.textContent = cliques;
+
   const nome = document.getElementById('nome').value;
   const email = document.getElementById('email').value;
   const nascimento = document.getElementById('nascimento').value;
   const telefone = document.getElementById('telefone').value;
+  let idade = calcaularIdade(nascimento);
 
-  adicionarPaciente(nome, email, nascimento, telefone);
+  adicionarPaciente(nome, email, nascimento, telefone, idade);
   renderizarTabela();
 
   formulario.reset(); // limpa os campos do formulário
